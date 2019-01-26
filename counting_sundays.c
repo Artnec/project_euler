8e2637a511eb06a49a04ee5521cdcb7992b6fda4
Artem NESTERUK <artnec@outlook.com>
2019-01-26 19:56:24 +0200

    added solution to problem 19

---
+++ counting_sundays.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+
+/*
+** You are given the following information, but you may prefer to do some research for yourself.
+
+**   1 Jan 1900 was a Monday.
+**   Thirty days has September,
+**   April, June and November.
+**   All the rest have thirty-one,
+**   Saving February alone,
+**   Which has twenty-eight, rain or shine.
+**   And on leap years, twenty-nine.
+**   A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+
+** How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+*/
+
+int		main(void)
+{
+	int sundays = 0;
+	for (int y = 1; y < 101; y++)
+	{
+		for (int m = 0; m < 12; m++)
+		{
+			struct tm date = {.tm_year = y, .tm_mon = m, .tm_mday = 1};
+			mktime(&date);
+			if (date.tm_wday == 0)
+				sundays++;
+		}
+	}
+	printf("%d\n", sundays);
+}
