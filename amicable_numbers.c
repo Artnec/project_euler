663533ab21ede93e7624f4cb395795891bd833af
Artem NESTERUK <artnec@outlook.com>
2019-01-27 22:37:11 +0200

    added solution to problem 21

---
+++ amicable_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+** Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+** If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+** For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+** The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+** Evaluate the sum of all the amicable numbers under 10000.
+*/
+
+#define MAX 10000
+
+int		main(void)
+{
+	int nums[MAX] = {0};
+	for (int i = 2; i < MAX; i++)
+	{
+		nums[i] += 1;
+		for (int n = i + i; n < MAX; n += i)
+			nums[n] += i;
+	}
+
+	int sum = 0;
+	for (int i = 2; i < MAX; i++)
+	{
+		if (nums[i] < MAX && i != nums[i] && i == nums[nums[i]])
+			sum += i;
+	}
+	printf("%d\n", sum);
+}
